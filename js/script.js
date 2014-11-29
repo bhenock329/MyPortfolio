5af4434a8f4f8ea2973cc2f74c326762ebc5d100
bhenock329 <brian.henock@gmail.com>
2014-11-29 11:47:26 -0500

    Header & .js Updates

--- js/script.js
+++ js/script.js
@@ -1,37 +1,7 @@
 /**************************************************** 
-					Page Flyouts
+          Fixed Header 
 ****************************************************/
 
-$(function() {
-  $('.work-icon').on('click', function() {
-    $('.wrap').toggleClass('flyout');
-    $('.work-wrap').toggleClass("active");
-  });
-});
-
-
-$(function() {
-  $('.logo').on('click', function() {
-    $('.wrap').removeClass('flyout');
-  });
-});
-
-
-$(function() {
-  $('.info-icon').on('click', function() {
-    $('.wrap').toggleClass('flyout');
-    $('.info-wrap').toggleClass("active");
-  });
-});
-
-$(function() {
-  $('.logo').on('click', function() {
-    $('.wrap').removeClass('flyout');
-  });
-});
-/**************************************************** 
-					Fixed Header 
-****************************************************/
 $(window).bind('scroll', function() {
    var pos = $(window).scrollTop();
    if (pos > 500)  { 
@@ -41,3 +11,40 @@ $(window).bind('scroll', function() {
        $('.fixedheader').removeClass('fixed');
    }
 });
+
+/**************************************************** 
+					Page Flyouts
+****************************************************/
+      
+      /*******************************************
+                    WORK FLYOUT
+      *******************************************/
+      $(function() {
+        $('.work-icon').on('click', function() {
+          $('.wrap').toggleClass('flyout');
+          $('.work-wrap').toggleClass("active");
+        });
+      });
+
+      $(function() {
+        $('.logo').on('click', function() {
+          $('.wrap').removeClass('flyout');
+        });
+      });
+
+      /*******************************************
+                    NFO FLYOUT
+      *******************************************/
+      $(function() {
+        $('.info-icon').on('click', function() {
+          $('.wrap').toggleClass('flyout');
+          $('.info-wrap').toggleClass("active");
+        });
+      });
+
+      $(function() {
+        $('.logo').on('click', function() {
+          $('.wrap').removeClass('flyout');
+        });
+      });
+
