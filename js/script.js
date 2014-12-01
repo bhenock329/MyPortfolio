d329a8f984052fafbab07811bc26538fbcb0458c
bhenock329 <brian.henock@gmail.com>
2014-12-01 18:46:14 -0500

    Functionality

--- js/script.js
+++ js/script.js
@@ -1,50 +1,56 @@
-/**************************************************** 
-          Fixed Header 
-****************************************************/
+$ (document).ready(function(){
 
-$(window).bind('scroll', function() {
-   var pos = $(window).scrollTop();
-   if (pos > 500)  { 
-       $('.fixedheader').addClass('fixed');
-   }
-   else  {
-       $('.fixedheader').removeClass('fixed');
-   }
+  /**************************************************** 
+            Fixed Header 
+  ****************************************************/
+
+  $(window).bind('scroll', function() {
+     var pos = $(window).scrollTop();
+     if (pos > 500)  { 
+         $('.fixedheader').addClass('fixed');
+     }
+     else  {
+         $('.fixedheader').removeClass('fixed');
+     }
+  });
+
+  /**************************************************** 
+  					Page Flyouts
+  ****************************************************/
+        
+        /*******************************************
+                      WORK FLYOUT
+        *******************************************/
+        $(function() {
+          $('.work-icon').on('click', function() {
+            $('.work-wrap').toggleClass("active");
+          });
+          $('.logo').on('click', function() {
+            $('.work-wrap').removeClass('active');
+          });
+          $('.work-icon').on('click', function() {
+            $('.work-wrap.active').slideToggle('.active slow');
+          });
+        });
+        $(function() {
+          $('.work-icon').on('click', function() {
+            $('.work-wrap.active').slideToggle('.active slow');
+          });
+        });
+     
+
+        /*******************************************
+                      NFO FLYOUT
+        *******************************************/
+        $(function() {
+          $('.info-icon').on('click', function() {
+            $('.info-wrap').toggleClass("active");
+          });
+        });
+         
+        $(function() {
+          $('.logo').on('click', function() {
+            $('.info-wrap').removeClass('active');
+          });
+        });
 });
-
-/**************************************************** 
-					Page Flyouts
-****************************************************/
-      
-      /*******************************************
-                    WORK FLYOUT
-      *******************************************/
-      $(function() {
-        $('.work-icon').on('click', function() {
-          $('.wrap').toggleClass('flyout');
-          $('.work-wrap').toggleClass("active");
-        });
-      });
-
-      $(function() {
-        $('.logo').on('click', function() {
-          $('.wrap').removeClass('flyout');
-        });
-      });
-
-      /*******************************************
-                    NFO FLYOUT
-      *******************************************/
-      $(function() {
-        $('.info-icon').on('click', function() {
-          $('.wrap').toggleClass('flyout');
-          $('.info-wrap').toggleClass("active");
-        });
-      });
-
-      $(function() {
-        $('.logo').on('click', function() {
-          $('.wrap').removeClass('flyout');
-        });
-      });
-
